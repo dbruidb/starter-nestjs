c9d8cb0dc3fe97a71da86ffc94917176124406c1
Ontaneda Adrian <dbruid98760@gmail.com>
2024-05-05 17:43:39 -0500

    games fixed2

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,8 @@ import { AppModule } from './app.module';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ["*"],
-    methods: ["GET", "POST"],
-    credentials: true
-    
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
+
 
   const port = process.env.SERVER_PORT
   await app.listen(port);
