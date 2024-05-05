78bdc0f2240125582074e0ba4123c6d4e38bc79d
Ontaneda Adrian <dbruid98760@gmail.com>
2024-05-05 17:46:17 -0500

    games fixed22

--- dist/main.js
+++ dist/main.js
@@ -3,7 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors({
+        origin: ["http://localhost:4200/"],
+        methods: ["GET", "POST"],
+        credentials: true
+    });
     const port = process.env.SERVER_PORT;
     await app.listen(port);
 }
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,14 @@ import { AppModule } from './app.module';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-
+  const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: ["http://localhost:4200/"],
+    methods: ["GET", "POST"],
+    credentials: true
+    
+  });
 
   const port = process.env.SERVER_PORT
   await app.listen(port);
