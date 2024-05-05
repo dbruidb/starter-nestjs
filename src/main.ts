f005a73ac1de373dc48312cd0ea06d410d629f2c
Ontaneda Adrian <dbruid98760@gmail.com>
2024-05-05 17:34:44 -0500

    games fixed2

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { AppModule } from './app.module';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);/*
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ["http://nrex-league.x10.mx"],
+    origin: ["http://nrex-league.x10.mx", "*"],
     methods: ["GET", "POST"],
     credentials: true
     
   });
-*/
+
   const port = process.env.SERVER_PORT
   await app.listen(port);
 
